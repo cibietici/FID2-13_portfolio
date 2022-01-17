9c35adf8fd6f42ef647d2baa3da8057a5998bf09
carlo alberto burato <carloalberto.burato@gmail.com>
2022-01-17 20:18:16 +0100

    Add hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+function init() {
 
+    const burgerIcon = document.querySelector('.burger-icon')
+    const mobileNav = document.querySelector('.main-menu-mobile')
+    burgerIcon.addEventListener('click', () => {
+        mobileNav.classList.toggle('hidemobileview')
+    })
+
+    const closeMobileNav = document.querySelector('.close-mobile-nav')
+    closeMobileNav.addEventListener('click', () => {
+        mobileNav.classList.toggle('hidemobileview')
+    })
+}
+
+init ();

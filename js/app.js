1f97f525eead78bf1a50fce997283cb3918d3276
carlo alberto burato <carloalberto.burato@gmail.com>
2022-01-19 11:35:37 +0100

    Full hamburger menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+function init() {
+    const burgerIcon = document.querySelector('.burger-icon');
+    const mobileNav = document.querySelector('.mobile-nav');
+    burgerIcon.addEventListener('click', () => {
+        mobileNav.classList.toggle('mobile-nav-hide');
+        burgerIcon.classList.toggle('burger');
+        burgerIcon.classList.toggle('closemobilemenu');
+    });
+}
 
+init();

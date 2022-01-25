c1fd510aff80b47f43be0ddfdd115f23cd9b3e3d
carlo alberto burato <carloalberto.burato@gmail.com>
2022-01-25 11:37:45 +0100

    Syntax fixed

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ function init() {
         burgerIcon.classList.toggle('burger');
         burgerIcon.classList.toggle('closemobilemenu');
     });
-
     getPosts();
 }
 
@@ -16,28 +15,29 @@ async function getPosts() {
     `);
     const { result } = await posts.json();
 
-    const worksList = document.querySelector('.workslist')
+    const worksList = document.querySelector('.workslist');
+
+    const cdnUrl = 'https://cdn.sanity.io/images/p5snqp28/production/';
 
     result.forEach(post => {
+        const cover = post.mainImage.asset._ref.split('-');
+
         const workBlock = document.createElement('a');
         workBlock.classList.add('work');
-        workBlock.setAttribute('href', '#');
+        workBlock.setAttribute('href', `./work.html?page=${post.slug.current}`);
         const workTitle = document.createElement('h2');
         workTitle.classList.add('work-title');
-        workTitle.innerText = post.title
+        workTitle.innerText = post.title;
         workBlock.append(workTitle);
         const workMask = document.createElement('div');
         workMask.classList.add('work-mask');
         workBlock.append(workMask);
         const workCover = document.createElement('img');
-        workCover.setAttribute('src', './assets/thumb02.jpg')
+        workCover.setAttribute('src', `${cdnUrl}${cover[1]}-${cover[2]}.${cover[3]}`);
         workCover.classList.add('work-cover');
-        workBlock.append(workCover)
-        console.log(workBlock)
-        worksList.append(workBlock)
+        workBlock.append(workCover);
+        worksList.append(workBlock);
     });
-
-    console.log(result)
 
 }
 

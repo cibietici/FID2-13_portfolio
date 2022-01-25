ed88895d4a0e800c71523856fb514adf8e62b714
carlo alberto burato <carloalberto.burato@gmail.com>
2022-01-25 10:56:23 +0100

    Connection to sanity.io

--- js/app.js
+++ js/app.js
@@ -6,6 +6,39 @@ function init() {
         burgerIcon.classList.toggle('burger');
         burgerIcon.classList.toggle('closemobilemenu');
     });
+
+    getPosts();
+}
+
+async function getPosts() {
+    const posts =  await fetch(`https://p5snqp28.api.sanity.io/v1/data/query/production?query=*
+    [_type == "post"]
+    `);
+    const { result } = await posts.json();
+
+    const worksList = document.querySelector('.workslist')
+
+    result.forEach(post => {
+        const workBlock = document.createElement('a');
+        workBlock.classList.add('work');
+        workBlock.setAttribute('href', '#');
+        const workTitle = document.createElement('h2');
+        workTitle.classList.add('work-title');
+        workTitle.innerText = post.title
+        workBlock.append(workTitle);
+        const workMask = document.createElement('div');
+        workMask.classList.add('work-mask');
+        workBlock.append(workMask);
+        const workCover = document.createElement('img');
+        workCover.setAttribute('src', './assets/thumb02.jpg')
+        workCover.classList.add('work-cover');
+        workBlock.append(workCover)
+        console.log(workBlock)
+        worksList.append(workBlock)
+    });
+
+    console.log(result)
+
 }
 
 init();
